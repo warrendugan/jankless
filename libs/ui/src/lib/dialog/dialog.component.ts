e081a57d2de290bad4c051b4a4cf8a61894f51ba
Warren Dugan <warrendugan@gmail.com>
2021-05-10 13:41:09 -0700

    Add animation to dialog. Close on window click.

--- libs/ui/src/lib/dialog/dialog.component.ts
+++ libs/ui/src/lib/dialog/dialog.component.ts
@@ -1,4 +1,6 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { fromEvent, Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
@@ -7,31 +9,32 @@ import { filter } from 'rxjs/operators';
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fade', [
+      transition('void => *', [style({ opacity: 0 }), animate(250, style({ opacity: 1 }))]),
+      transition('* => void', [animate(250, style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class DialogComponent implements OnInit, OnDestroy {
-  @ViewChild('content') _: any;
-  public windowClicks = fromEvent(document, 'click').pipe(
-    filter((click) => {
-      console.log(click);
-      return click.type === 'click';
-    }),
-  );
-  private destroy: Subscription[] = [];
-
-  constructor() {}
-
+  @HostBinding('@fade') fade = true;
   @HostBinding('tabindex') tabindex = '-1';
   @HostBinding('role') role = 'dialog';
+  @ViewChild('content') _: any;
+  private destroy: Subscription[] = [];
+  private name = 'DIALOG';
+  public windowClicks = fromEvent(document, 'click').pipe(
+    filter(({ target }) => (target as any).nodeName === this.name),
+  );
 
-  handleClick(event: any) {
-    if (event.type === 'click') {
-      console.log(event.toElement);
-      console.log(this._);
-    }
-  }
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    this.destroy.push(this.windowClicks.subscribe(this.handleClick));
+    this.destroy.push(this.windowClicks.subscribe(this.close.bind(this)));
+  }
+
+  close() {
+    this.router.navigate([{ outlets: { modal: null } }]);
   }
 
   ngOnDestroy() {
