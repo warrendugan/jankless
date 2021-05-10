a0a0bc3e052a65961711ef5a83ac62ae786c462c
Warren Dugan <warrendugan@gmail.com>
2021-05-10 14:32:49 -0700

    Clean up dialog.

--- libs/ui/src/lib/dialog/dialog.component.ts
+++ libs/ui/src/lib/dialog/dialog.component.ts
@@ -1,5 +1,5 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { fromEvent, Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
@@ -18,12 +18,10 @@ import { filter } from 'rxjs/operators';
 })
 export class DialogComponent implements OnInit, OnDestroy {
   @HostBinding('@fade') fade = true;
-  @HostBinding('tabindex') tabindex = '-1';
-  @HostBinding('role') role = 'dialog';
-  @ViewChild('content') _: any;
+
   private destroy: Subscription[] = [];
   private name = 'DIALOG';
-  public windowClicks = fromEvent(document, 'click').pipe(
+  private windowClicks = fromEvent(document, 'click').pipe(
     filter(({ target }) => (target as any).nodeName === this.name),
   );
 
