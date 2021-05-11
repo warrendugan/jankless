d335fc7cd25170b15ed797f2cc87586c5909d245
Warren Dugan <warrendugan@gmail.com>
2021-05-10 20:54:19 -0700

    Refactor focus trap.

--- libs/ui/src/lib/focus-trap/focus-trap.directive.ts
+++ libs/ui/src/lib/focus-trap/focus-trap.directive.ts
@@ -5,33 +5,24 @@ import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
 })
 export class FocusTrapDirective implements AfterViewInit {
   private $ = this.el.nativeElement;
-  private get inputs(): any[] {
-    return this.$.querySelectorAll('a[href], button, textarea, input, select');
+  private get focus(): Node[] {
+    return Array.from(this.$.querySelectorAll('a[href], button, textarea, input, select') as NodeList).filter(
+      (el: any) => !el.disabled,
+    );
   }
-  private get focus(): any[] {
-    return Array.from(this.inputs).filter((el: any) => !el.disabled);
+  private get first(): HTMLInputElement {
+    return this.focus[0] as HTMLInputElement;
   }
-  private get first(): any {
-    return this.focus[0];
+  private get last(): HTMLInputElement {
+    return this.focus[this.focus.length - 1] as HTMLInputElement;
   }
-  private get last(): any {
-    return this.focus[this.focus.length - 1];
-  }
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    this.trapFocus();
-  }
-
-  trapFocus() {
-    if (!this.inputs) return;
-    if (!this.focus.length) return;
-
     this.first.focus();
-
     this.renderer.listen(this.$, 'keydown', (e) => {
-      const isTabPressed = e.keyCode === 9;
-      if (!isTabPressed) return;
+      if (e.keyCode !== 9) return;
 
       if (e.shiftKey) {
         if (document.activeElement === this.first) {
